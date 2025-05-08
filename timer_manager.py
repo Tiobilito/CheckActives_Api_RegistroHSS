4c732decf942d5c2fee5808c3f965d3ca627efb9
André Josué Marín González <andre.marin1135@alumnos.udg.mx>
2025-05-07 21:07:41 -0600

    Cambio a webSockets

--- timer_manager.py
+++ timer_manager.py
@@ -1,20 +1,5 @@
 import threading
-from db import update_user_status
-from config import TIMEOUT, AUTH_TOKEN_EXPIRATION
-
-# Para actividad de usuarios
-active_timers = {}
-timer_lock = threading.Lock()
-
-def timer_callback(user_id: str):
-    with timer_lock:
-        print(f"Timer expirado para {user_id}")
-        if user_id in active_timers:
-            del active_timers[user_id]
-    try:
-        update_user_status(user_id, False)
-    except Exception as e:
-        print(f"Error en timer_callback para {user_id}: {str(e)}")
+from config import AUTH_TOKEN_EXPIRATION
 
 # Para tokens de autenticación
 active_auth_tokens = {}
